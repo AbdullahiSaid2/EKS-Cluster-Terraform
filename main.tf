37450625cf412c43ddc05f02dcda3d0676dfd2f0
Abdullahi Said <63651100+AbdullahiSaid2@users.noreply.github.com>
2022-08-26 16:07:23 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,94 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.27.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = var.region
+  access_key = "AKIAYA7XNH5JQJNXGTZ3"
+  secret_key = "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"
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_availability_zones" "available" {
+
+}
+
+
+resource "aws_security_group" "worker_group_mgmt_one" {
+  name_prefix = "worker_group_mgmt_one"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+    ]
+
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.2"
+
+  name                 = "test-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
+  }
+
+}
+
+module "eks" {
+  source                          = "terraform-aws-modules/eks/aws"
+  version                         = "18.28.0"
+  cluster_name                    = var.cluster_name
+  subnet_ids                      = module.vpc.private_subnets
+  cluster_timeouts                = var.cluster_timeouts
+  cluster_endpoint_private_access = true
+  vpc_id                          = module.vpc.vpc_id
+
+  eks_managed_node_group_defaults = {
+    disk_size      = 50
+    instance_types = ["m6i.large", "m5.large"]
+  }
+
+  eks_managed_node_groups = {
+    blue = {}
+    green = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 3
+
+      instance_types = ["t3.large"]
+    }
+  }
+}
